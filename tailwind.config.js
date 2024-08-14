383d5df327501c49e7d476e37abe65c38d98cf9e
Mary Lou D. Cortez <maryloucortez12@gmail.com>
2024-08-14 18:07:43 +0800

    display position grid and flex

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "twitter-blue" : "#1DA1F2",
+      },
+    },
+  },
+  plugins: [],
+}
+
